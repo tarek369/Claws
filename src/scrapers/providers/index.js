34e1ffe2c83ca2a43f9957e0bdf84078786f1e6e
NBTX <nbtx@telegenic.me>
2019-01-26 11:58:39 +0000

    Re-enable other scrapers

--- src/scrapers/providers/index.js
+++ src/scrapers/providers/index.js
@@ -3,18 +3,18 @@
 const providers =
 	{
 		movies: [
-            /* require('./movies/Afdah'),
+            require('./movies/Afdah'),
             require('./movies/AZMovies'),
             require('./movies/bfmovies'),
             require('./movies/StreamM4u'),
-			require('./movies/MovieFiles'), */
+			require('./movies/MovieFiles'),
 		],
 		tv: [
-			/*require('./tv/GoWatchSeries'),
+			require('./tv/GoWatchSeries'),
 			require('./tv/SeriesFree'),
 			require('./tv/AfdahTV'),
 			require('./tv/Series8'),
-			require('./tv/SwatchSeries'), */
+			require('./tv/SwatchSeries'),
 		],
 		universal: [
 			require('./universal/123movie'),
