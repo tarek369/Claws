e9c50306d2d8dcf859a8b377c1b92c2fc2e5a628
claws-dev <nxtvserverdev@protonmail.com>
2019-02-26 10:31:50 -0500

    Uncommented providers

--- src/scrapers/providers/index.js
+++ src/scrapers/providers/index.js
@@ -2,23 +2,23 @@
 
 module.exports = exports = {
     movies: [
-        // require('./movies/Afdah'),
-        // require('./movies/AZMovies'),
-        // require('./movies/bfmovies'),
-        // require('./movies/StreamM4u'),
-        // require('./movies/MovieFiles'),
+        require('./movies/Afdah'),
+        require('./movies/AZMovies'),
+        require('./movies/bfmovies'),
+        require('./movies/StreamM4u'),
+        require('./movies/MovieFiles'),
     ],
     tv: [
-        // require('./tv/GoWatchSeries'),
+        require('./tv/GoWatchSeries'),
         require('./tv/SeriesFree'),
-        // require('./tv/SwatchSeries'),
-        // new (require('./tv/series8'))()
+        require('./tv/SwatchSeries'),
+        new (require('./tv/series8'))()
     ],
     anime: [
-        // new (require('./anime/MasterAnime'))(),
+        new (require('./anime/MasterAnime'))(),
     ],
     universal: [
-        // require('./universal/123movie'),
-        // new (require('./anime/MasterAnime'))()
+        require('./universal/123movie'),
+        new (require('./anime/MasterAnime'))()
     ]
 };
