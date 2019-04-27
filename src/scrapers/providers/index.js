b0236495bfb356b87f4653b0a52e3b157d10ae18
Teekzilla <47798620+Teekzilla@users.noreply.github.com>
2019-04-27 13:30:08 -0400

    Add UltraHDIndir (WIP)

--- src/scrapers/providers/index.js
+++ src/scrapers/providers/index.js
@@ -3,44 +3,44 @@ const { queue } = require('../../utils/queue');
 
 module.exports.providers = {
     anime: [
-        new (require('./anime/AnimePahe'))(),
+        // new (require('./anime/AnimePahe'))(),
     ],
     movies: [
-        new (require('./movies/Afdah'))(queue),
-        new (require('./movies/AZMovies'))(queue),
-        new (require('./movies/BFMovies'))(queue),
-        new (require('./movies/DLFilm'))(queue),
-        new (require('./movies/MeliMedia'))(queue),
-        new (require('./movies/StreamM4u'))(queue)
+        // new (require('./movies/Afdah'))(queue),
+        // new (require('./movies/AZMovies'))(queue),
+        // new (require('./movies/BFMovies'))(queue),
+        // new (require('./movies/DLFilm'))(queue),
+        // new (require('./movies/MeliMedia'))(queue),
+        // new (require('./movies/StreamM4u'))(queue)
     ],
     tv: [
-        new (require('./tv/MoviesWeb'))(queue),
-        new (require('./tv/ProjectFreeTV'))(queue),
-        new (require('./tv/SeriesFree'))(queue),
-        new (require('./tv/SwatchSeries'))(queue)
+        // new (require('./tv/MoviesWeb'))(queue),
+        // new (require('./tv/ProjectFreeTV'))(queue),
+        // new (require('./tv/SeriesFree'))(queue),
+        // new (require('./tv/SwatchSeries'))(queue)
     ],
     universal: [
-        new (require('./universal/123Movie'))(queue),
+        // new (require('./universal/123Movie'))(queue),
         // new (require('./universal/DebugDummy'))(queue),
-        new (require('./universal/FardaDL'))(queue),
-        new (require('./universal/GoWatchSeries'))(queue),
-        new (require('./universal/HeyDL'))(queue),
-        new (require('./universal/ODB'))(queue),
-        new (require('./universal/Onmovies'))(queue),
-        new (require('./universal/Series8'))(queue),
-        new (require('./universal/Series9'))(queue),
-        new (require('./universal/SockShare'))(queue),
-        new (require('./universal/SolarMovie'))(queue)
+        // new (require('./universal/FardaDL'))(queue),
+        // new (require('./universal/GoWatchSeries'))(queue),
+        // new (require('./universal/HeyDL'))(queue),
+        // new (require('./universal/ODB'))(queue),
+        // new (require('./universal/Onmovies'))(queue),
+        // new (require('./universal/Series8'))(queue),
+        // new (require('./universal/Series9'))(queue),
+        // new (require('./universal/SockShare'))(queue),
+        // new (require('./universal/SolarMovie'))(queue)
     ],
     rd: {
         movies: [
-
+            new (require('./rd/movies/UltraHDIndir'))(queue)
         ],
         tv: [
 
         ],
         universal: [
-            new (require('./rd/universal/TwoDDL'))(queue)
+            // new (require('./rd/universal/TwoDDL'))(queue)
         ]
     }
 };
---
+++ src/scrapers/providers/rd/movies/UltraHDIndir.js
@@ -0,0 +1,88 @@
+const cheerio = require('cheerio');
+const BaseProvider = require('../../BaseProvider');
+const Utils = require('../../../../utils/index');
+
+module.exports = class UltraHDIndir extends BaseProvider {
+    /** @inheritdoc */
+    getUrls() {
+        return ['http://ultrahdindir.com'];
+    }
+
+    /** @inheritdoc */
+    async scrape(url, req, ws) {
+        const imdb_id = req.query.imdb_id;
+        const hasRD = req.query.hasRD;
+        const resolvePromises = [];
+        let headers = {};
+
+        try {
+            const searchUrl = `${url}/index.php?do=search`;
+            const rp = this._getRequest(req, ws);
+            const jar = rp.jar();
+            const response = await this._createRequest(rp, searchUrl, jar, headers,
+                {
+                    method: 'POST',
+                    formData: {
+                        'do': 'search',
+                        'subaction': 'search',
+                        'search_start': 0,
+                        'full_search': 1,
+                        'result_from': 1,
+                        'story': imdb_id
+                    }
+                });
+
+            let $ = cheerio.load(response);
+
+            console.log($.html());
+
+            // .quote a:not([href*='http://ultrahdindir.com'])
+
+            let moviePageArray = $('.box-out').toArray().reduce((returnArray, element) => {
+                let foundIMDB = $(element).find(`.news-text a[href*='${imdb_id}']`).attr('href');
+                let foundPage = $(element).find('.news-head .news-title a').attr('href');
+
+                if (foundIMDB) {
+                    returnArray.push(foundPage);
+                }
+
+                return returnArray;
+            }, []);
+
+            // let isSecondarySearch = false;
+            // if (type == 'tv' && moviePageArray.length == 0) {
+            //     isSecondarySearch = true;
+
+            //     searchTitle = searchTitle.substr(0, searchTitle.length - 3);
+            //     searchUrl = this.generateSearchURL(url, searchTitle);
+            //     const secondaryResponse = await this._createRequest(rp, searchUrl, jar, headers);
+
+            //     moviePageArray = this.getSearchResults(searchTitle, year, secondaryResponse);
+            // }
+
+            // if (moviePageArray.length == 0) {
+            //     return Promise.resolve();
+            // }
+
+            // for (let moviePage of moviePageArray) {
+            //     let moviePageHTML = await this._createRequest(rp, moviePage, jar, headers);
+
+            //     let $ = cheerio.load(moviePageHTML);
+
+            //     $('.postpage_movie_download_area').toArray().forEach(element => {
+            //         let releaseName = $(element).find('.rele_name').text();
+            //         if (!isSecondarySearch || (isSecondarySearch && releaseName.includes(`.${season}${paddedEpisode}.`))) {
+            //             $(element).find('.anch_multilink a').toArray().forEach(linkElement => {
+            //                 let hostLink = $(linkElement).attr('href');
+            //                 let quality = Utils.getQualityInfo(releaseName);
+            //                 resolvePromises.push(this.resolveLink(hostLink, ws, jar, headers, quality, { isDDL: false }, hasRD));
+            //             });
+            //         }
+            //     });
+            // };
+        } catch (err) {
+            this._onErrorOccurred(err)
+        }
+        return Promise.all(resolvePromises)
+    }
+}
